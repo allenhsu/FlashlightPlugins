5a6b03190630e45e24d35c2009f8450b8ecf4447
Allen Hsu <me@imallen.com>
2014-11-17 13:34:16 +0800

    async network request for kuaidi 100

--- kuaidi.bundle/plugin.py
+++ kuaidi.bundle/plugin.py
@@ -9,91 +9,15 @@ import random
 def results(parsed, original_query):
     number = parsed.get('number', '')
     number = re.sub(r'[^0-9]', '', number)
-    if not number:
-        return {}
-    url = 'http://www.kuaidi100.com/autonumber/auto?num=%s' % number
-    response = urllib2.urlopen(url).read()
-    prediction = json.loads(response)
-    if not prediction or type(prediction) != type([]):
-        return {}
-    comCode = prediction[0]['comCode']
-    url = 'http://www.kuaidi100.com/query?type=%(comCode)s&postid=%(number)s&id=1&valicode=&temp=%(temp)s' % dict(comCode=comCode, number=number, temp=random.random())
-    response = urllib2.urlopen(url).read()
-    result = json.loads(response)
-    data = result.get('data')
-    style = u'''
-    <style>
-    * {
-        padding: 0;
-        margin: 0;
-    }
-    html, body, body > div {
-        margin: 0;
-        width: 100%;
-        height: 100%;
-        font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial;
-        line-height: 1.2;
-    }
-    h1, h2, h3, h4, h5 {
-        font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "HiraginoSansGB-W6", "Hiragino Sans GB W6";
-    }
-    h1 {
-        font-size: 32px;
-    }
-    h1.number {
-        border-bottom: #ddd 1px solid;
-        padding: 0px 0px 10px 0px;
-        margin: 0px 0px 10px 0px;
-        color: #444;
-    }
-    h3 {
-        font-size: 15px;
-        color: #888;
-    }
-    div.content {
-        padding: 15px;
-    }
-    ul, li {
-        list-style: none;
-    }
-    li {
-        font-size: 15px;
-        color: #888;
-        padding: 3px 0;
-    }
-    .time {
-        font-size: 12px;
-        color: #aaa;
-    }
-    .context {
-        font-size: 15px;
-        color: #444;
-    }
-    .current .context {
-        font-weight: bold;
-        color: #444;
-    }
-    </style>
-    '''
-    content = '<ul>'
-    last_status = ''
-    i = 0
-    for item in data:
-        if not last_status:
-            last_status = item.get('context', '')
-        content = content + '<li class="%s"><p class="time">%s</p><p class="context">%s</p></li>' % ('current' if i == 0 else '', item.get('time'), item.get('context'))
-        i += 1
-    content = content + '</ul>'
-    html = u'<div class="body"><div class="content"><h1 class="number">%(number)s</h1><div class="data">%(content)s</div></div></div>' % dict(number=number, content=content)
-    url = 'http://www.kuaidi100.com/chaxun?com=%s&nu=%s' % (comCode, number)
+    html = open("kuaidi.html").read().replace("<!--NUMBER-->", number)
     return {
-        "title": u'%s（回车在快递 100 查看更多信息）' % number,
-        "html": '%s%s' % (style, html),
-        "run_args": [url],
+        "title": u'快递 100 搜索 "%s" 结果' % number,
+        "html": html,
+        "run_args": [number],
         "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
         "webview_links_open_in_browser": True,
     }
 
 def run(url):
     if url:
-        os.system('open "{0}"'.format(url))
+        os.system('open "http://m.kuaidi100.com/result.jsp?nu={0}"'.format(url))
